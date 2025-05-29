4046fff7bb5e7049bb371b3c69b1f6c5984ea7c0
Aobing Yin <ay1523@ic.ac.uk>
2025-05-29 15:51:01 +0100

    FIFO tested correctly

---
+++ Web_test.py
@@ -0,0 +1,14 @@
+import os, json, time
+
+FIFO_CMD   = '/tmp/robot-cmd'
+
+if not os.path.exists(FIFO_CMD):
+    os.mkfifo(FIFO_CMD)
+
+while True:
+    with open(FIFO_CMD, 'r') as fifo:
+        for ch in fifo.read():  # 读单字符
+            if not ch:
+                continue
+            print(ch)
+    time.sleep(0.005)
